7ac2025c9ed72673112b7a04717a5955988b1044
Hanw20 <harrenwijaya@gmail.com>
2026-04-14 13:09:22 +0700

    page awal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,15 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.coroutinesCore)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
